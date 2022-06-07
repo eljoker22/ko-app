09f1fbed3f59b9d18393649546094ce7765fbb4f
eljoker22 <eljokermano50@gmail.com>
2022-06-07 22:00:10 +0200

    commit

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,6 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
