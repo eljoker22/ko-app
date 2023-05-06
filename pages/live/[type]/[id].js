2d4c396b6f469444152af37fc5d6fd8c9c774f09
eljoker22 <eljokermano50@gmail.com>
2023-05-07 01:31:20 +0200

    test navigate

--- pages/live/[type]/[id].js
+++ pages/live/[type]/[id].js
@@ -18,14 +18,14 @@ export async function getServerSideProps(ctx) {
     const { type, id } = ctx.query;
     const token = nookies.get(ctx);
 
-    // if (!token.jwt) {
-    //     return{
-    //         redirect: {
-    //             destination: '/login',
-    //             permanent: false,
-    //         }
-    //     }
-    // }
+    if (!token.jwt) {
+        return{
+            redirect: {
+                destination: '/login',
+                permanent: false,
+            }
+        }
+    }
 
     // get url with dynamic content
     // const urlReq = type === 'football-live' ?  
@@ -40,25 +40,25 @@ export async function getServerSideProps(ctx) {
     const match = type === 'football-live' ? await getMatch(id) : type === 'box-video' ? await getBoxMatch(id) : await getUfcMatch(id);
 
         
-    //     // get user data
-    //         const resUser = await fetch(`${process.env.nodeAppApi}/v1/auth/user`, {
-    //             method: 'get',
-    //             headers: {
-    //                 "x-access-token": `${token?.jwt}`
-    //             }
-    //         }) 
-    //         const userData = await resUser.json();
+        // get user data
+            const resUser = await fetch(`${process.env.nodeAppApi}/v1/auth/user`, {
+                method: 'get',
+                headers: {
+                    "x-access-token": `${token?.jwt}`
+                }
+            }) 
+            const userData = await resUser.json();
     
     
     
-    //     if (userData.error || !userData.user) {
-    //         return{
-    //             redirect: {
-    //                 destination: '/login',
-    //                 permanent: false,
-    //             }
-    //         }
-    //     }
+        if (userData.error || !userData.user) {
+            return{
+                redirect: {
+                    destination: '/login',
+                    permanent: false,
+                }
+            }
+        }
         
         let notAllow = false;
 
@@ -66,7 +66,7 @@ export async function getServerSideProps(ctx) {
     const { user } = userData;
 
     if (!match?.fields.free) { // if match not free check access user allow or not
-
+        
     }
 
     
