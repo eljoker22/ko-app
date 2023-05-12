231e88851779fc0b9eae4685d573b15b87cfe727
eljoker22 <eljokermano50@gmail.com>
2023-05-12 15:41:18 +0200

    fix login system 1

--- next.config.js
+++ next.config.js
@@ -11,20 +11,14 @@ const nextConfig = {
   images: {
     domains: ['cdn.contentful.com', 'images.ctfassets.net'],
   },
-  async headers() {
+  async rewrites() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
+        source: '/api/:path*',
+        destination: 'https://ko-sports-users.onrender.com/:path*',
+      },
     ]
-  }
+  },
 }
 
 module.exports = nextConfig
