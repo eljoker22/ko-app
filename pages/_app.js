6563bc288ecad7e4a66dcae6fc89c6ac69b1bdd0
eljoker22 <eljokermano50@gmail.com>
2023-06-27 20:03:15 +0200

    add google analitycs

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,21 @@ import '../styles/globals.css'
 import Layout from '../componnet/Layout';
 import { Provider } from 'react-redux'
 import {store} from '../state/store';
+import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
     <Layout>
+      <Script strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=G-7T0SQ669WG"></Script>
+      <Script strategy='afterInteractive'>
+        {
+          `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-7T0SQ669WG');`
+        }
+      </Script>
       <Component {...pageProps} />
     </Layout>
     </Provider>
